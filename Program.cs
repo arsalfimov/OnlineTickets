f66d9d89adbb0b301b24c5dbdb785eae6db7bb48
arsalfimov <Alfimov.ars@gmail.com>
2025-03-12 18:23:58 +0300

    Add connection string and migrations

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 using OnlineTickets.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
  
